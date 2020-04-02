74f826a1b350557f221fece1d138b313a532a0fe
Biola <abiola2326@gmail.com>
2020-04-02 16:42:46 +0100

    Added production build

---
+++ build/precache-manifest.5347812ed661047f8dfd60189f8e0807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b1219bb96419a4292bb7f226318c50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3ee5036e4bf0a0f1eed",
+    "url": "/static/css/main.d1c129fe.chunk.css"
+  },
+  {
+    "revision": "46b642ea1c3efc575f71",
+    "url": "/static/js/2.43325b7e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.43325b7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3ee5036e4bf0a0f1eed",
+    "url": "/static/js/main.db363f2a.chunk.js"
+  },
+  {
+    "revision": "4ee863eec7039f899cc1",
+    "url": "/static/js/runtime-main.64cf2cba.js"
+  }
+]);
--- index.js
+++ index.js
@@ -2,23 +2,20 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+app.use(express.static('build'));
 app.use(cors());
 
 app.use(express.json());
 
-// const requestLogger = (request, response, next) => {
-//   console.log('Method:', request.method);
-//   console.log('Path:  ', request.path);
-//   console.log('Body:  ', request.body);
-//   console.log('---');
-//   next();
-// };
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+};
 
-// const unknownEndpoint = (request, response) => {
-//   response.status(404).send({ error: 'unknown endpoint' });
-// };
-
-// app.use(requestLogger);
+app.use(requestLogger);
 
 let notes = [
   {
