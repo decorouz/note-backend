06631f69f242ec3a653f335beecf27f86fc88fa4
Biola <abiola2326@gmail.com>
2020-04-09 02:24:02 +0100

    Implemented Eslint

--- models/note.js
+++ models/note.js
@@ -9,7 +9,7 @@ console.log('connecting to', url);
 mongoose
   .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => {
-    console.log('connected to MongoDB');
+    console.log(result, 'connected to MongoDB');
   })
   .catch(error => {
     console.log('error connecting to MongoDB:', error.message);
