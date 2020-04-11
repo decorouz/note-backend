65fa8600eedb5407335c2ff1ffdc5db8b97c3335
Biola <abiola2326@gmail.com>
2020-04-11 14:43:15 +0100

    updated models module

--- models/note.js
+++ models/note.js
@@ -2,22 +2,6 @@
 /* eslint-disable no-underscore-dangle */
 const mongoose = require('mongoose')
 
-mongoose.set('useFindAndModify', false)
-
-// eslint-disable-next-line no-undef
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose
-  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => {
-    console.log(result, 'connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: { type: String, minlength: 5, required: true },
   date: { type: Date, required: true },
