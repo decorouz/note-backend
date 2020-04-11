5acd4559122cfb80c09f24140a00101d7283503d
Biola <abiola2326@gmail.com>
2020-04-11 01:09:34 +0100

    fixed esllint errors

--- mongo.js
+++ mongo.js
@@ -1,46 +1,37 @@
-const mongoose = require('mongoose');
+/* eslint-disable no-shadow */
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log('give password as argument');
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const url = process.env.MONGODB_URI
 
-const url = `mongodb+srv://deco_fullstack:${password}@cluster0-1je7l.mongodb.net/note-app?retryWrites=true&w=majority`;
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const noteSchema = new mongoose.Schema({
   content: String,
   date: Date,
-  important: Boolean
-});
+  important: Boolean,
+})
 
-const Note = mongoose.model('Note', noteSchema);
-
-const content = process.argv[3];
+const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-  content: `${content}`,
+  content: 'Callback-function suck',
   date: new Date(),
-  important: true
-});
+  important: true,
+})
 
-/*
-Fetching Objects from the database
-*/
-if (process.argv.length > 3) {
-  note.save().then(response => {
-    console.log('note saved!', response.content);
-    mongoose.connection.close();
-  });
-  return;
-}
+note.save().then((response) => {
+  console.log('note saved!', response.content)
+  mongoose.connection.close()
+})
 
-Note.find({ important: true }).then(response => {
-  response.forEach(note => {
-    console.log(note);
-  });
-  mongoose.connection.close();
-});
+Note.find({ important: true }).then((response) => {
+  response.forEach((note) => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
